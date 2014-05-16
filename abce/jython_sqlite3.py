d20feef54b61fb0e98ae432fb14d0b949281de68
Davoud Taghawi-Nejad <davoud@taghawi-nejad.de>
2014-05-16 18:36:21 -0400

    postprocess.py from fullpython, no .R and no R in setup.py

--- abce/jython_sqlite3.py
+++ abce/jython_sqlite3.py
@@ -3,13 +3,14 @@ try:
     from sqlite3 import * #pylint: disable=W0614
 
 
-    class Connect:
+    class CustomConnect:
         def __init__(self, directory, db_name):
             self.db = sqlite3.connect(directory + '/' + db_name + '.db')
             self.cursor = self.db.cursor()
 
         def execute(self, command):
             self.cursor.execute(command)
+            self.description = self.cursor.description
 
         def executeQuery(self, command):
             return self.cursor.execute(command)
@@ -17,6 +18,16 @@ try:
         def column_names(self, table_name):
             self.cursor.execute("""PRAGMA table_info(""" + table_name + """)""")
             return [row[1] for row in self.cursor]
+
+        def table_ids(self, table_name):
+            self.cursor.execute("""PRAGMA table_info(""" + table_name + """)""")
+            return [row[0] for row in self.cursor]
+
+        def fetchall(self):
+            return self.cursor.fetchall()
+
+        def fetchone(self):
+            return self.cursor.fetchone()
 
         def commit(self):
             self.db.commit()
@@ -34,12 +45,13 @@ except ImportError:
     import org.sqlite.SQLiteDataSource as SQLiteDataSource #pylint: disable=F0401
 
 
-    class Connect:
+    class CustomConnect:
         def __init__(self, directory, db_name):
             dataSource = SQLiteDataSource()
             dataSource.setUrl("jdbc:sqlite:" + directory + '/' + db_name + '.db')
             self.connection = dataSource.getConnection()
             self.cursor = self.connection.createStatement()
+            self.describtion = self.db.describtion
 
         def execute(self, command):
             self.cursor.execute(command)
@@ -55,6 +67,21 @@ except ImportError:
                 if not(table_info.next()):
                     break
             return columns
+
+        def table_ids(self, table_name):
+            table_info = self.cursor.executeQuery("""PRAGMA table_info(""" + table_name + """)""")
+            columns = []
+            while True:
+                columns.append(table_info.getString(1))
+                if not(table_info.next()):
+                    break
+            return columns
+
+        def fetchall(self):
+            return self.cursor.fetchall()
+
+        def fetchone(self):
+            return self.cursor.fetchone()
 
         def commit(self):
             try:
--- abce/postprocess.py
+++ abce/postprocess.py
@@ -1,51 +1,74 @@
-try:
-    import rpy2.robjects
-    import os
+"""
+functions:
+sam_cell(seller, buyer, cell='quantity'), returns you the evolution over time of a cell
+trade_over_time(seller, buyer, good, cell='quantity'), returns you the evolution over time of a cell
+sam(t), returns the social accounting matrix at time t
+sam_ext(t), returns the social accounting matrix at time t for every individual agent
+"""
+import os
+import pylab
+import numpy
+import abce.jython_sqlite3 as sqlite3
+import csv
 
 
-    def to_r_and_csv(directory, basepath, csv=True):
-        go = rpy2.robjects.r
-        os.chdir(directory)
-        print(__file__)
-        go("source('%s/postprocess.R')" % basepath)
+def to_r_and_csv(directory, db_name, csv=True): #pylint: disable=R0914
+    os.chdir(directory)
+    #db_name = db_name + '.db'
+    c = sqlite3.CustomConnect('.', db_name)
+    import glob
+    print('--')
+    print(db_name)
+    print glob.glob("*")
+    print glob.glob(db_name)
+    table_names = c.executeQuery("SELECT name FROM sqlite_master WHERE type='table'").fetchall()
+    table_names = [i for sublist in table_names for i in sublist] # equivalent to unlist, or numpy's ravel
 
-        all_tables = go('ls()')
-        all_tables = [table for table in all_tables]
-        try:
-            all_tables.remove('trade_unified')
-        except ValueError:
-            pass
-        new_tables = set()
-        for table in all_tables:
-            names = table.split('_')
-            if len(names) > 1:
-                new_tables.add(names[0])
-                name = '_'.join(table.split('_')[1:])
-                columns = go("names(%s)" % table)
-                columns = [col for col in columns if not(col in ('round', 'id'))]
-                for col in columns:
-                    go("names(%s)[names(%s)=='%s'] <- '%s_%s'" %
-                                (table, table, col, name, col))
+    print("Import:")
+    for i in table_names:
+        c.execute("select * from %s" % i)
+        table_contents = c.fetchall()
+        column_names = [j[0] for j in c.description]
+        column_types = [type(k) for k in table_contents[-1]]  # hack-ish, could have been done better @Rudy, I think this is pythonic
+        print zip(column_names, column_types)
+        print table_contents[1-3]
 
-        for table in new_tables:
-            acquisitions = go("ls(pattern='%s_.*')" % table)
-            acquisitions = [acq for acq in acquisitions]
-            go("%s <- %s[c('round', 'id')]" % (table, acquisitions[0]))
-            for acq in acquisitions:
-                go("%s <- merge(%s, %s, by=c('round', 'id'))"
-                            % (table, table, acq))
-                go("rm(%s)" % acq)
+    for table in table_names:
+        table_to_file(table, column_names, table_contents)
 
-        all_tables = go('ls()')
-        if csv:
-            for table in all_tables:
-                go("write.csv2(get('%s'), file='%s.csv')" % (table, table))
+    for table in table_names:
+        aggregate_and_convert(table, c)
 
-        for table in new_tables:
-            go("%s$round <- factor(%s$round)" % (table, table))
-            go("%s$id <- factor(%s$id)" % (table, table))
+    try: #MONKY PATCH why does this not work in python???, I numpy is not available in JYTHON, so NameError-try/catch is necessary
+        tables = numpy.recarray(table_contents, dtype=zip(column_names, column_types)) #pylint: disable=E1101
+    except (TypeError, ValueError) as e:
+        print("irrelevant - numpy.recarray error: ", e)
+        tables = [dict([(key, cell) for key, cell in zip(column_names, row)]) for row in table_contents]
+    except NameError:
+        tables = [dict([(key, cell) for key, cell in zip(column_names, row)]) for row in table_contents]
+    c.close()
 
-        go("save(list = ls(all=TRUE), file = '%s')" % 'database.RData')
-except ImportError:
-    def to_r_and_csv(directory, basepath, csv=True):
-        pass
+
+def table_to_file(table_name, column_names, table_contents):
+    with open(os.path.join('.', table_name + '.csv'), 'w') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(column_names)
+        for row in table_contents:
+            csv_writer.writerow(row)
+
+def aggregate_and_convert(table, cursor):
+    cursor.execute('SELECT * FROM %s' % table)
+    column_names = [column_name[0] for column_name in cursor.description]
+    if 'id' in column_names:
+        with open(os.path.join('.', table + '_aggregate.csv'), 'w') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            column_names.remove('id')
+            column_names.remove('round')
+            sum_string = ','.join('sum(%s)' % item for item in column_names)
+            cursor.execute('SELECT round, ' + sum_string +' FROM ' + table + ' GROUP BY round;')
+            csv_writer.writerow(['round'] + column_names)
+            while True:
+                try:
+                    csv_writer.writerow(cursor.fetchone())
+                except csv.Error:
+                    break
